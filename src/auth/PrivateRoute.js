756eeaf3d5827ca24378f47a629462c12959575a
Sak0240 <saketdeshmukh0240@gmail.com>
2026-03-04 00:16:36 +0530

    removed src

--- src/auth/PrivateRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function PrivateRoute({ children }) {
-  const isAuth = localStorage.getItem("auth");
-
-  return isAuth ? children : <Navigate to="/login" />;
-}
